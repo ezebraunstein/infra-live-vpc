93e0d56cee174789acbbc18ef6b3679c328eab6e
ezequiel <ezequiel.braunstein@gmail.com>
2024-11-20 16:41:44 -0300

    :art:Modify Backend repo

--- backend/main.tf
+++ backend/main.tf
@@ -1,11 +1,11 @@
 module "s3_backend" {
-  source      = "git::https://github.com/ezebraunstein/module-backend.git//modules/s3"
+  source      = "git::https://github.com/ezebraunstein/module-vpc.git//modules/s3"
   bucket_name = var.bucket_name
   tags        = var.tags
 }
 
 module "dynamo_backend" {
-  source     = "git::https://github.com/ezebraunstein/module-backend.git//modules/dynamo"
+  source     = "git::https://github.com/ezebraunstein/module-vpc.git//modules/dynamo"
   table_name = var.table_name
   tags       = var.tags
 }
